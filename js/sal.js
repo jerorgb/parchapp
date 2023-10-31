e4212dc7567a47166fba722adb5e8ad90830e2ee
jerorgb <jrestrepogiraldo1@gmail.com>
2023-10-31 14:36:30 -0500

    ALL UPLOADED

---
+++ js/sal.js
@@ -0,0 +1,20 @@
+
+
+
+var txt = "ABC123";
+
+var num = 30;
+var float = 3.0;
+var yn = true;
+var string = "tremendo";
+
+console.log("number " + num + " " + typeof(num));
+console.log("float", float + " " + typeof(float));
+console.log("name " + string + " " + typeof(string));
+console.log("yn", yn + " " + typeof(yn));
+// alert("Alert TXT " + txt);
+
+
+
+
+
